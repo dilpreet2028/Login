23bfa13ba7ccc97f4d47b64c365d132b7177cb84
dilpreet96 <dilpreet2028@gmail.com>
2015-07-17 12:59:16 +0530

    Used Volley

--- app/src/main/java/com/example/login/MainActivity.java
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -15,6 +15,14 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 import org.apache.http.client.ClientProtocolException;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -29,15 +37,18 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.lang.reflect.Method;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 public class MainActivity extends AppCompatActivity {
-
+    String urlstr;
     EditText user,pass;
     String username,password;
     @Override
@@ -51,7 +62,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void login(View view){
-        String urlstr;
+
         username=user.getText().toString();
         password=pass.getText().toString();
         //~
@@ -60,7 +71,7 @@ public class MainActivity extends AppCompatActivity {
         urlstr="http://dilpreet-app.herokuapp.com/login";
         //urlstr="http://localhost:5000/login";
         //urlstr="https://aqueous-forest-8384.herokuapp.com/login.php";
-        URL url;
+       /* URL url;
         try{
             url=new URL(urlstr);
             new GetData().execute(url);
@@ -68,89 +79,56 @@ public class MainActivity extends AppCompatActivity {
         }
         catch (MalformedURLException e){
 
-        }
+        }*/
+        requestToServer();
     }
 
     public void signup(View v){
         startActivity(new Intent(getApplicationContext(),Signup.class));
     }
-    public class GetData extends AsyncTask<URL,Void,Integer>{
 
+    public void requestToServer(){
+        RequestQueue requestQueue= Volley.newRequestQueue(this);
 
-        @Override
-        protected Integer doInBackground(URL... params) {
+        StringRequest myreq=new StringRequest(Request.Method.POST, urlstr,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        String checkpass=parseJSON(response);
+                        if(checkpass!=null){
+                            int result=verifyPass(checkpass,password);
+                            if(result==1)
+                                Toast.makeText(MainActivity.this,"Logged in",Toast.LENGTH_LONG).show();
 
-            HttpURLConnection httpURLConnection=null;
-            InputStream inputStream=null;
-            StringBuilder builder=new StringBuilder();
-            int success=1,result=0;
-
-            try {
-                httpURLConnection=(HttpURLConnection) params[0].openConnection();
-                httpURLConnection.setRequestMethod("POST");
-                httpURLConnection.setDoInput(true);
-                httpURLConnection.setDoOutput(true);
-                //httpURLConnection.setRequestProperty("Content-Type", "application/json");
-
-                httpURLConnection.connect();
-
-                String query = String.format("username=%s&password=%s",
-                        URLEncoder.encode(username, "UTF-8"),
-                        URLEncoder.encode(password, "UTF-8"));
-
-
-
-                OutputStream writer=new BufferedOutputStream(httpURLConnection.getOutputStream());
-                writer.write(query.getBytes());
-                writer.flush();
-                writer.close();
-
-                
-                success=httpURLConnection.getResponseCode();
-
-                if(success==HttpURLConnection.HTTP_OK){
-                    inputStream=httpURLConnection.getInputStream();
-                    InputStreamReader reader=new InputStreamReader(inputStream);
-                    BufferedReader bufferedReader=new BufferedReader(reader);
-                     builder=new StringBuilder();
-                    String line;
-
-                    while((line=bufferedReader.readLine())!=null){
-                        builder.append(line);
+                        }
+                        else{
+                            Toast.makeText(MainActivity.this,"Try Again...",Toast.LENGTH_LONG).show();
+                        }
                     }
-
+                }
+                ,
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MainActivity.this,"error :"+error,Toast.LENGTH_LONG).show();
+                    }
                 }
 
-                String checkpass=parseJSON(builder.toString());
-                Log.d("My",checkpass+"   "+builder.toString()+ "   "+password);
+        ){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
 
-                if(checkpass!=null){
-                    result=verifyPass(password,checkpass);
-                    Log.d("My",result+"");
-                }
-
+                Map<String,String> params=new HashMap<String,String>();
+                params.put("username",username);
+                params.put("password",password);
+                return params;
             }
-            catch (Exception e){
-                success=0;
-            }
+        };
 
+        requestQueue.add(myreq);
 
-            finally {
-                httpURLConnection.disconnect();
-            }
-
-          return result;
-        }
-
-        @Override
-        protected void onPostExecute(Integer result) {
-            super.onPostExecute(result);
-            if(result==1)
-                Toast.makeText(getApplicationContext(),"Logged in",Toast.LENGTH_LONG).show();
-            else
-                Toast.makeText(getApplicationContext(),"try again",Toast.LENGTH_LONG).show();
-        }
     }
+
 
 
     ////Converting the json object to the required format.
--- app/src/main/java/com/example/login/Signup.java
+++ app/src/main/java/com/example/login/Signup.java
@@ -12,6 +12,14 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -24,12 +32,15 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class Signup extends AppCompatActivity {
 
     String username,password;
     EditText user,pass;
+    String urlstr;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,96 +50,55 @@ public class Signup extends AppCompatActivity {
     }
 
     public void create(View view){
-        String urlstr;
+
         username=user.getText().toString();
         password=pass.getText().toString();
 
         urlstr="http://dilpreet-app.herokuapp.com/signup";
         //urlstr="http://localhost:5000/signup";
+        requestToServer();
 
-        URL url;
-        try{
-            url=new URL(urlstr);
-            new GetData().execute(url);
-
-        }
-        catch (MalformedURLException e){
-
-        }
     }
 
-    public class GetData extends AsyncTask<URL,Void,Integer> {
-
-
-        @Override
-        protected Integer doInBackground(URL... params) {
-
-            HttpURLConnection httpURLConnection=null;
-            InputStream inputStream=null;
-            StringBuilder builder=new StringBuilder();
-            int success=1,result=0;
-
-            try {
-                httpURLConnection=(HttpURLConnection) params[0].openConnection();
-                httpURLConnection.setRequestMethod("POST");
-                httpURLConnection.setDoInput(true);
-                httpURLConnection.setDoOutput(true);
-                httpURLConnection.connect();
-
-                String query = String.format("username=%s&password=%s",
-                        URLEncoder.encode(username, "UTF-8"),
-                        URLEncoder.encode(password, "UTF-8"));
-
-
-
-                OutputStream writer=new BufferedOutputStream(httpURLConnection.getOutputStream());
-                writer.write(query.getBytes());
-                writer.flush();
-                writer.close();
-
-
-                success=httpURLConnection.getResponseCode();
-
-                if(success==HttpURLConnection.HTTP_OK){
-                    inputStream=httpURLConnection.getInputStream();
-                    InputStreamReader reader=new InputStreamReader(inputStream);
-                    BufferedReader bufferedReader=new BufferedReader(reader);
-                    builder=new StringBuilder();
-                    String line;
-
-                    while((line=bufferedReader.readLine())!=null){
-                        builder.append(line);
+    public void requestToServer(){
+        RequestQueue queue= Volley.newRequestQueue(Signup.this);
+        StringRequest request=new StringRequest(Request.Method.POST, urlstr,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        checkResponse(response);
                     }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
 
-                }
+                    }
+                }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
 
-               JSONObject json=new JSONObject(builder.toString());
-                result=json.getInt("success");
-
+                Map<String,String> params=new HashMap<String,String>();
+                params.put("username",username);
+                params.put("password",password);
+                return params;
             }
-            catch (Exception e){
-                success=0;
-            }
+        };
+        queue.add(request);
 
+    }
 
-            finally {
-                httpURLConnection.disconnect();
-            }
-
-            return result;
-        }
-
-        @Override
-        protected void onPostExecute(Integer result) {
-            super.onPostExecute(result);
-            if(result==1){
-                Toast.makeText(getApplicationContext(), "Signed up", Toast.LENGTH_LONG).show();
-                startActivity(new Intent(getApplicationContext(),MainActivity.class));
-                finish();
-            }
-
+    private void checkResponse(String response){
+        try{
+            JSONObject json=new JSONObject(response);
+            int result=json.getInt("success");
+            if(result==1)
+                Toast.makeText(Signup.this,"Succesfully Signed Up",Toast.LENGTH_SHORT).show();
             else
-                Toast.makeText(getApplicationContext(),"try again",Toast.LENGTH_LONG).show();
+                Toast.makeText(this,"Unable to Signup",Toast.LENGTH_SHORT).show();
+        }
+        catch (JSONException e){
+            e.printStackTrace();
         }
     }
 
